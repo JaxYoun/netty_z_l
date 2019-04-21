8e8198a929ad5e37d08f466cd4233e2c653d9598
JaxYoun <gao88jie@qq.com>
2019-04-21 18:05:16 +0800

    添加了部分心跳机制的代码

---
+++ src/main/java/heartbeat/server/HeartBeatInitializer.java
@@ -0,0 +1,18 @@
+package heartbeat.server;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+
+/**
+ * @author: Yang
+ * @date: 2019/1/8 00:28
+ * @description:
+ */
+public class HeartBeatInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast();
+    }
+}
